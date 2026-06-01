73dc34244beb4cb1ae8539b3e65557edf1a4218b
River <river@bulls-media.com>
2026-06-01 11:32:24 +0300

    Initial Mbito project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <h1>Mbito</h1>
+      <p>Retool-style internal app builder</p>
+    </div>
+  );
+}
+
+export default App;
